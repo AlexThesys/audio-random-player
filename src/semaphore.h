38f471b9bcdfc5fa20f8182c382a4cd10887936c
alexthesys <alexsuprunov@gmail.com>
2024-12-07 15:36:28 +0100

    Add counting semaphore

--- src/semaphore.h
+++ src/semaphore.h
@@ -24,3 +24,39 @@ public:
 		cv.notify_one();
 	}
 };
+
+class semaphore_counting
+{
+	std::mutex m;
+	std::condition_variable cv;
+	volatile int counter = 0;
+	int max_count = 1; // binary semaphore by default
+public:
+	void set_max_count(int count) {
+		max_count = count;
+	}
+	void wait() {
+		std::unique_lock<std::mutex> lock(m);
+		if (counter == 0) {
+			cv.wait(lock, [this]() { return counter != 0; });
+			counter--;
+		}
+	}
+	void signal() {
+		{
+			std::unique_lock<std::mutex> lock(m);
+			if (counter < max_count) {
+				counter++;
+			}
+		}
+		cv.notify_one();
+	}
+	void signal(int count) {
+		{
+			std::unique_lock<std::mutex> lock(m);
+			const int new_count = counter + count;
+			counter = (new_count < max_count) ? new_count : max_count;
+		}
+		cv.notify_all();
+	}
+};
